3497695d09968a5aab568a1a728741fe33b40216
{Krzysiek} <stkr1@live.com>
2015-06-02 15:15:18 +0200

    added EBook class

---
+++ app/src/main/java/com/stepienk/libraryapp/model/dropbox_ebooks/EBook.java
@@ -0,0 +1,64 @@
+package com.stepienk.libraryapp.model.dropbox_ebooks;
+
+/**
+ * Created by Krzysiek on 2015-04-20.
+ * eBook object for holding all data
+ */
+public class EBook {
+    private String id;
+    private String title;
+    private String desc;
+    private String pubDate;
+    private String link;
+    private String pdfLink;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getPubDate() {
+        return pubDate;
+    }
+
+    public void setPubDate(String pubDate) {
+        this.pubDate = pubDate;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getPdfLink() {
+        return pdfLink;
+    }
+
+    public void setPdfLink(String pdfLink) {
+        this.pdfLink = pdfLink;
+    }
+
+
+}
